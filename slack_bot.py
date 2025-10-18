d6826298b3014d31656b69fca33a19d032fbe5e1
LukeUhlir <uhlirluke@gmail.com>
2025-10-18 13:38:25 -0500

    deleted message functionality + repeat message

--- slack_bot.py
+++ slack_bot.py
@@ -2,10 +2,34 @@ import slack
 import os
 from pathlib import Path
 from dotenv import load_dotenv
+from flask import Flask
+from slackeventsapi import SlackEventAdapter
 
 env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
 
-client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
+app = Flask(__name__)
+slack_event_adapter = SlackEventAdapter( os.environ['SIGNING_SECRET'],'/slack/events',app)
 
-client.chat_postMessage(channel=('#all-test-space'), text='Hello World!')
+client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
+BOT_ID = client.api_call("auth.test")['user_id']
+
+@slack_event_adapter.on('message')
+def message(payload):
+    event = payload.get('event', {})
+    channel_id = event.get('channel')
+    user_id = event.get('user')
+    text = event.get('text')
+    subtype = event.get('subtype')
+    if channel_id != 'C09M7V67NEA':
+        if subtype == "message_deleted":
+            deleted_message = event.get('previous_message')
+            text = deleted_message.get('text')
+            client.chat_postMessage(channel='#bot-logs', text=("Message Deleted: " + text))
+            print(event)
+
+        if user_id != BOT_ID:
+            client.chat_postMessage(channel=channel_id, text=text)
+
+if __name__ == "__main__":
+    app.run(debug=True)
