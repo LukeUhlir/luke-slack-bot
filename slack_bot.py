f9aad2d4e846ba804ff255876368341e277cb514
LukeUhlir <uhlirluke@gmail.com>
2025-10-19 12:17:17 -0500

    added message moderation functionality

--- slack_bot.py
+++ slack_bot.py
@@ -5,6 +5,17 @@ from dotenv import load_dotenv
 from flask import Flask
 from slackeventsapi import SlackEventAdapter
 
+def containsNaughtyWord(word):
+    naughtyWords = ["shit", "fuck", "nigger", "faggot", "fuk", "fuc" ]
+    for swear_word in naughtyWords:
+        result = word.find(swear_word)
+        if result != -1:
+            return result
+
+def deleteMessage(channel_id, time_stamp):
+    admin_client = slack.WebClient(token=os.environ['ADMIN_TOKEN'])
+    admin_client.chat_delete(channel=channel_id, ts=time_stamp)
+
 env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
 
@@ -18,18 +29,22 @@ BOT_ID = client.api_call("auth.test")['user_id']
 def message(payload):
     event = payload.get('event', {})
     channel_id = event.get('channel')
-    user_id = event.get('user')
-    text = event.get('text')
     subtype = event.get('subtype')
     if channel_id != 'C09M7V67NEA':
         if subtype == "message_deleted":
             deleted_message = event.get('previous_message')
             text = deleted_message.get('text')
-            client.chat_postMessage(channel='#bot-logs', text=("Message Deleted: " + text))
-            print(event)
+            user_id = deleted_message.get('user')
+            client.chat_postMessage(channel='#bot-logs', text=("Message Deleted: " + text +"\n"+user_id))
+        else:
+            text = event.get('text')
+            print(containsNaughtyWord(text))
+            if containsNaughtyWord(text) != None:
+                ts = event.get('ts')
+                client.chat_postMessage(channel='#bot-logs', text="Bad message detected")
+                deleteMessage(channel_id, ts)
 
-        if user_id != BOT_ID:
-            client.chat_postMessage(channel=channel_id, text=text)
 
 if __name__ == "__main__":
     app.run(debug=True)
+
