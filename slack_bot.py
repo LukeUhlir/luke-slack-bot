d0d2451acbdba6cfb24035ce1da0ac609812b11a
LukeUhlir <uhlirluke@gmail.com>
2025-10-19 14:16:19 -0500

    first implementation of a slash command

--- slack_bot.py
+++ slack_bot.py
@@ -3,7 +3,7 @@ import os
 import re
 from pathlib import Path
 from dotenv import load_dotenv
-from flask import Flask
+from flask import Flask, request, Response
 from slackeventsapi import SlackEventAdapter
 
 def prepForModeration(word):
@@ -33,6 +33,8 @@ slack_event_adapter = SlackEventAdapter( os.environ['SIGNING_SECRET'],'/slack/ev
 client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
 BOT_ID = client.api_call("auth.test")['user_id']
 
+message_counts = {}
+
 @slack_event_adapter.on('message')
 def message(payload):
     event = payload.get('event', {})
@@ -46,11 +48,27 @@ def message(payload):
             client.chat_postMessage(channel='#bot-logs', text=("Message Deleted: " + text +"\n"+user_id))
         else:
             text = event.get('text')
+            user_id = event.get('user')
+            print(user_id)
             if containsNaughtyWord(text) != None:
                 ts = event.get('ts')
                 client.chat_postMessage(channel='#bot-logs', text="Bad message detected")
                 deleteMessage(channel_id, ts)
+            else:
+                if user_id in message_counts:
+                    message_counts[user_id] += 1
+                else: 
+                    message_counts[user_id] = 1
 
+@app.route('/message-count', methods=['POST'])
+def message_count():
+    data = request.form
+    user_id = data.get('user_id')
+    channel_id = data.get('channel_id')
+    message_count = message_counts.get(user_id, 0)
+
+    client.chat_postMessage(channel=channel_id, text=f"Message: {message_count}")
+    return Response(), 200
 
 if __name__ == "__main__":
     app.run(debug=True)
